e955319c4788c68dceb7fbe28ed89b6558a21300
Peter Zagubisalo <peter.zagubisalo@gmail.com>
2019-04-06 16:38:58 +0700

    Update linus_libertini.bat

--- linus_libertini.bat
+++ linus_libertini.bat
@@ -1,5 +1,7 @@
 @echo First clone libertinus repo to the ../
+@echo First install FontForge to default location in %%ProgramFiles(x86)%%
 @echi Don't forget to pull the latest commit from libertinus repo
+@echo Don't forget to update FontForge
 @pause
 @chcp 65001 > NUL
 @set PYTHONIOENCODING=utf-8
