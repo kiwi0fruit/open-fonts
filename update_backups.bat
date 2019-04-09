f6fd756a7d8ac246df4ff1bbc7a2c239e09cba15
kiwi0fruit <peter.zagubisalo@gmail.com>
2019-04-09 13:22:27 +0700

    commit

---
+++ update_backups.bat
@@ -0,0 +1,27 @@
+@git pull
+@cd ..
+@git clone --depth=1 https://github.com/dejavu-fonts/dejavu-fonts && cd dejavu-fonts || cd dejavu-fonts && git pull
+@cd ..
+@git clone https://github.com/stipub/stixfonts && cd stixfonts || cd stixfonts && git pull
+@cd ..
+@git clone https://github.com/latofonts/lato-source && cd lato-source || cd lato-source && git pull
+@cd ..
+@git clone https://github.com/adobe-fonts/source-sans-pro && cd source-sans-pro || cd source-sans-pro && git pull
+@cd ..
+@git clone --depth=1 https://github.com/google/fonts && cd fonts || cd fonts && git pull
+@cd ..
+@git clone https://github.com/adobe-fonts/source-serif-pro && cd source-serif-pro || cd source-serif-pro && git pull
+@cd ..
+@git clone https://github.com/adobe-fonts/source-code-pro && cd source-code-pro || cd source-code-pro && git pull
+@cd ..
+@git clone --depth=1 https://github.com/IBM/plex && cd plex || cd plex && git pull
+@cd ..
+@git clone https://github.com/FAlthausen/Vollkorn-Typeface && cd Vollkorn-Typeface || cd Vollkorn-Typeface && git pull
+@cd ..
+@git clone https://github.com/khaledhosny/libertinus && cd libertinus || cd libertinus && git pull
+@cd ..
+@git clone https://github.com/MihailJP/Inconsolata-LGC && cd Inconsolata-LGC || cd Inconsolata-LGC && git pull
+@cd ..
+@git clone https://github.com/cpitclaudel/monospacifier && cd monospacifier || cd monospacifier && git pull
+@cd ..
+@pause
