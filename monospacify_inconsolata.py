ddb3867c91d11e899492bb305d1a37432f937e2d
kiwi0fruit <peter.zagubisalo@gmail.com>
2020-10-13 16:19:45 +0700

    upd inconsolata sugar

--- monospacify_inconsolata.py
+++ monospacify_inconsolata.py
@@ -12,7 +12,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import subprocess
 from monospacifier_api import monospacifier
 from helper import rename_font
 import os
@@ -21,14 +20,14 @@ from os import path as p
 
 here = p.dirname(p.abspath(__file__))
 repos = p.dirname(here)
-inconsolata = p.join(here, 'Fonts', 'Inconsolata')
+inconsolata = p.join(here, 'Fonts', 'Inconsolata', 'Inconsolata')
 
 merge = False
 copy_metrics = True
 remove = (
     # ---- Bad dashes: ----
-    # [\u00AD \u1806 \uFE63 \uFF0D]
-    u'­', u'᠆',  u'﹣', u'－',
+    # [\u1806 \uFE63 \uFF0D \u00AD]
+    u'᠆',  u'﹣', u'－', u'­',
     # ---- OK dashes: ----
     # [u2E3A \u2E3B] (multiple of character width)
     # u'⸺', u'⸻',
@@ -36,38 +35,39 @@ remove = (
 spaces = (
     # ---- Bad whitespaces (dont't touch): ----
     # [\u1680]
-    # u' ',
+    u' ',
     # ---- Bad whitespaces: ----
     # [\u202F \u205F \u3000]
     u' ', u' ', u'　',
-    # ---- Bad whitespaces: ----
-    # [\u2000 \u2001 \u2002 \u2003 \u2004 \u2005 \u2006 \u2009 \u200A]
-    u' ', u' ', u' ', u' ', u' ', u' ', u' ', u' ', u' ',
 )
 
-dir_ = p.join(repos, '_InconsolataSugar')
+dir_ = p.join(repos, '_Inconsolatos')
 if not p.exists(dir_):
     os.makedirs(dir_)
 
+refs = [p.join(p.dirname(inconsolata), 'InconsolataLGC-Regular.sfd')]
 
-# Rename Source Code Pro:
-# ---------------------------------
+
+# Monospacify Inconsolata:
+# ------------------------
 styles = [
-    ('InconsolataLGC-Italic', 'Inconsolata LGC', 'Italic'),
-    ('InconsolataLGC-BoldItalic', 'Inconsolata LGC', 'Bold Italic'),
-    ('InconsolataLGC-Regular', 'Inconsolata LGC', 'Regular'),
-    ('InconsolataLGC-Bold', 'Inconsolata LGC', 'Bold'),
+    ('Inconsolata-Regular', 'Inconsolata', 'Regular'),
+    ('Inconsolata-Bold', 'Inconsolata', 'Bold'),
 ]
-shutil.copy(p.join(inconsolata, 'LICENSE'), p.join(dir_, 'LICENSE.txt'))
+fonts = [p.join(inconsolata, fn +'.ttf') for fn, ff, st in styles]
+monospacifier(fonts, refs, dir_, merge, copy_metrics)
+shutil.copy(p.join(inconsolata, 'OFL.txt'), p.join(dir_, 'OFL.txt'))
 
 
-reps = (('Inconsolata LGC', 'Inconsolata Sugar'), ('InconsolataLGC', 'InconsolataSugar'))
-def rep(s): return s.replace('LGC', 'Sugar')
+# Rename monospacified Inconsolata to Inconsolatos (temporal name):
+# ---------------------------------
+reps = (('Inconsolata', 'Inconsolatos'),)
+def rep(s): return s.replace('Inconsolata', 'Inconsolatos')
 
 for fn, ff, style in styles:
-    clean_up = False
-    ref = p.join(inconsolata, fn + '.sfd')
-    of = ref  # Old Font
+    clean_up = True
+    ref = p.join(inconsolata, fn + '.ttf')
+    of = p.join(dir_, (fn if fn != 'Inconsolata-Regular' else 'Inconsolata') + '_monospacified_for_InconsolataLGC.ttf')  # Old Font
 
     rename_font(input=of, save_as=p.join(dir_, rep(fn) +'.ttf'),
                 fontname=rep(fn),  # Font Name
